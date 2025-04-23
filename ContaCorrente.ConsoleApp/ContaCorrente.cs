bf83ed5c989b8f1cd349f66d700fbf0abf1fdf85
ehachmann <ehachmann@hotmail.com>
2025-04-22 22:46:19 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ContaCorrente.ConsoleApp
+{
+    public class ContaCorrente
+    {
+        public int numeroConta;
+        public decimal limite;
+        public decimal saldo;
+        public decimal[] movimentacoes = new decimal[10];
+
+        public decimal Sacar(decimal valor)
+        {
+            saldo = saldo - valor;
+            return saldo;
+        }
+
+        public decimal Depositar(decimal valor)
+        {
+            saldo = saldo + valor;
+            return saldo;
+        }
+
+        public decimal TransferirPara(ContaCorrente contaDestino, decimal valor)
+        {
+            saldo = saldo - valor;
+            return saldo;
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.Write($"Número da conta: {numeroConta}");
+            Console.WriteLine();
+            Console.Write($"Saldo atual: {saldo}");
+            Console.WriteLine();
+            Console.Write($"Limite: {limite}");
+            Console.WriteLine();
+            Console.Write($"Movimentações: {movimentacoes}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+
+            conta1.saldo = 1000;
+            conta1.numeroConta = 11;
+            conta1.limite = 0;
+            conta1.movimentacoes = new decimal[10];
+
+            conta1.Sacar(200);
+            conta1.Depositar(300);
+            conta1.Depositar(500);
+
+            ContaCorrente conta2 = new ContaCorrente();
+
+            conta2.saldo = 300;
+            conta2.numeroConta = 12;
+            conta2.limite = 0;
+            conta2.movimentacoes = new decimal[10];
+
+            conta1.TransferirPara(conta2, 400);
+            conta1.ExibirExtrato();
+
+            Console.WriteLine();
+
+            conta2.ExibirExtrato();
+
+            Console.ReadKey();
+
+        }
+    }
+}
