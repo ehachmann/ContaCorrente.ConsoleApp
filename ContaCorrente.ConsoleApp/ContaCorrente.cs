5eb9c9379f284abf7c3f695d89b889ea484cacdb
ehachmann <ehachmann@hotmail.com>
2025-04-23 23:31:14 -0300

    Criação de registros para exibir as movimentações

--- ContaCorrente.ConsoleApp/ContaCorrente.cs
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -4,25 +4,32 @@ namespace ContaCorrente.ConsoleApp
     public class ContaCorrente
     {
         public int numeroConta;
+        public int registroDeMovimentacoes = 0;
         public decimal limite;
         public decimal saldo;
+        public decimal saldoInicial;
         public decimal[] movimentacoes = new decimal[10];
+
 
         public decimal Sacar(decimal valor)
         {
             saldo = saldo - valor;
+            movimentacoes[registroDeMovimentacoes] = - valor;
             return saldo;
         }
 
         public decimal Depositar(decimal valor)
         {
             saldo = saldo + valor;
+            movimentacoes[registroDeMovimentacoes] = valor;
             return saldo;
         }
 
         public decimal TransferirPara(ContaCorrente contaDestino, decimal valor)
         {
             saldo = saldo - valor;
+            contaDestino.saldo = contaDestino.saldo + valor;
+            movimentacoes[registroDeMovimentacoes] = - valor;
             return saldo;
         }
 
@@ -30,11 +37,21 @@ namespace ContaCorrente.ConsoleApp
         {
             Console.Write($"Número da conta: {numeroConta}");
             Console.WriteLine();
-            Console.Write($"Saldo atual: {saldo}");
-            Console.WriteLine();
             Console.Write($"Limite: {limite}");
             Console.WriteLine();
-            Console.Write($"Movimentações: {movimentacoes}");
+            Console.Write($"Saldo inicial: {saldoInicial}");
+            
+            Console.WriteLine();
+
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                if (movimentacoes[i] != 0)
+                {
+                    Console.WriteLine($"Movimentações: {movimentacoes[i]}");
+                }
+            }
+            
+            Console.Write($"Saldo Atual: {saldo}");
             Console.WriteLine();
         }
     }
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -6,23 +6,30 @@
         {
             ContaCorrente conta1 = new ContaCorrente();
 
-            conta1.saldo = 1000;
+            conta1.saldoInicial = conta1.saldo = 1200;
             conta1.numeroConta = 11;
-            conta1.limite = 0;
+            conta1.limite = 1000;
             conta1.movimentacoes = new decimal[10];
+            conta1.registroDeMovimentacoes = 0;
 
+            conta1.movimentacoes[0] = 1000;
             conta1.Sacar(200);
+            conta1.registroDeMovimentacoes = 1;
             conta1.Depositar(300);
+            conta1.registroDeMovimentacoes = 2;
             conta1.Depositar(500);
+            conta1.registroDeMovimentacoes = 3;
+
 
             ContaCorrente conta2 = new ContaCorrente();
-
-            conta2.saldo = 300;
+            
+            conta2.saldoInicial = conta2.saldo = 300;
             conta2.numeroConta = 12;
-            conta2.limite = 0;
+            conta2.limite = 1000;
             conta2.movimentacoes = new decimal[10];
 
             conta1.TransferirPara(conta2, 400);
+          
             conta1.ExibirExtrato();
 
             Console.WriteLine();
